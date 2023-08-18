e5d11dc91076ac9275e195c6a8cdf622ad819828
Pingfei Wu <48017321+LIIT0215@users.noreply.github.com>
2023-08-18 14:47:54 +0800

    Update dependency.sh

--- dependency.sh
+++ dependency.sh
@@ -4,7 +4,7 @@ set -ex
 
 echo "[*] dependencies for chipyard-1.3.0"
 
-# sudo apt update
+sudo apt update
 sudo apt install -y curl build-essential bison flex
 sudo apt install -y libgmp-dev libmpfr-dev libmpc-dev zlib1g-dev vim git default-jdk default-jre
 sudo apt install -y unzip zip 
